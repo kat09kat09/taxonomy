42afc9470be191e28fe544fbe05d096b334d24c7
kat09kat09 <kat09kat09@gmail.com>
2015-11-30 14:26:50 -0500

    implemented Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,15 @@
 var Bee = function() {
+	//this= object.create(Bee.prototype)
+	Grub.call(this);
+	this.age= 5;
+	this.color= 'yellow'; 
+	this.job= 'keep on growing';  
 };
+
+//Bee.prototype delegates to Grub.prototype (we can access methods in Grub.prototype)
+Bee.prototype= Object.create(Grub.prototype);
+
+Bee.prototype.constructor= Bee; 
+
+
 
